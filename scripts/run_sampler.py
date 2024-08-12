b784936e008be08677b6f830d2504e268448aee3
ronnyhdez <ronny.hernandezm@gmail.com>
2024-08-12 15:51:40 -0600

    Ref #155 start with the loop

--- scripts/run_sampler.py
+++ scripts/run_sampler.py
@@ -13,8 +13,6 @@ following steps:
 
 Parameters:
 
-- site: The path to the GEE asset containing the polygons.
-- batch_size: The number of polygons to process per batch.
 - polygon_collection: The feature collection of polygons from 
   the specified site.
 - total_polygons: The total number of polygons in the feature
@@ -36,3 +34,54 @@ Usage:
 
 Author: Ronny A. Hernández Mora
 """
+
+import json
+import os
+import pickle
+import sys
+import time
+
+import ee
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from utils.utils import initialize_gee, get_feature_collection
+
+# Start the process
+initialize_gee()
+batch_size = 20
+polygon_collection = get_feature_collection(
+    'projects/ee-ronnyale/assets/random_sample_1000_filtered_polygons')
+total_polygons = polygon_collection.size().getInfo()
+
+# Products to be processed
+image_collections = [
+    {"name": "LANDSAT/LC08/C02/T1_L2", "label": "LC08"},
+    {"name": "LANDSAT/LC09/C02/T1_L2", "label": "LC09"},
+    {"name": "COPERNICUS/S2_SR_HARMONIZED", "label": "S2"}        
+]
+
+for collection in image_collections:
+    image_collection_name = collection["name"]
+    label = collection["label"]
+
+    # Process each batch
+    for start_index in range(0, total_polygons, batch_size):
+        pickle_filename = f'time_series_{label}_batch_{start_index}.pkl'
+
+        # Check if pkl file for the batch already exists
+        if os.path.exists(pickle_filename):
+            print(f'Batch {start_index} for {label} already processed and saved. Skipping...')
+            continue
+        batch = polygon_collection.toList(batch_size, start_index)
+        batch_fc = ee.FeatureCollection(batch)
+        batch_asset_id = f'projects/ee-ronnyale/assets/temp_batch_{lable}_{start_index}'
+        task = ee.batch.Export.table.toAsset(
+            collection = batch_fc,
+            description = f'export_batch_{label}_{start_index}',
+            assetId = batch_asset_id
+        )
+        task.start()
+
+        
+
