269b63e0af2e384240e645443233930c8bd027ff
ronnyhdez <ronny.hernandezm@gmail.com>
2024-08-13 17:15:53 -0600

    Ref #142 add script documentation

--- scripts/create_sampler_assets.py
+++ scripts/create_sampler_assets.py
@@ -9,32 +9,15 @@ Also, it will match the abandoned wells with their
 respective reference buffers which also will be
 processed with the LEAF toolbox sampler.
 
-1. Initializes the GEE API
-2. Divides the asset into batches
-3. Export each batch as an asset
-4. Runs the sampler for each bacth
-5. Extracts the results and save them as pkl files
-
-Parameters:
-
-- polygon_collection: The feature collection of polygons from 
-  the specified site.
-- total_polygons: The total number of polygons in the feature
-  collection.
-- image_collections: A list of dictionaries containing the 
-  image collection names and labels.
-- batch_asset_id: The ID for the temporary batch asset in GEE.
-
 Outputs:
-- Pickle files: One pickle file per image collection and batch,
-  saved in the current directory.
+- Two assets in GEE. One for the filtered abandoned wells and
+  a second one for their reference buffers.
 
 Usage:
-- Run the script with the specified site and batch_size.
-- The script will automatically handle the processing of 
-  the batches and save the results.
-- If a batch has already been processed, it will skip that
-  batch to avoid duplication.
+- Run the script with the lates reclaimed sites version and
+  reference buffers.
+- The script will create all the filters, randomly select
+  1000 abandoned wells polygons and their reference buffers 
 
 Author: Ronny A. Hernández Mora
 """
